1c821f77b90299a2316885090e8758b0585b7ced
BOLIVAR\1053793623 <ivan.barreiro@segurosbolivar.com>
2022-08-01 02:06:12 -0500

    Agregar archivos de proyecto.

---
+++ Application/ServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    public static class ServiceExtensions
+    {
+        //Agrupar las inyecciones o matricualas de nuestros servicios
+
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            //Registre automaticamente los mapeos que se hagan en esta biblioteca de clases
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            //Registre automaticamente las validaciones que se hagan en esta biblioteca de clases
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            //Registre automaticamente los mediadores que se implementen en esta biblioteca de clases
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+    }
+}
