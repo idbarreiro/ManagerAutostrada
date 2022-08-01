3d763ba3ab85ceaf27a3420aa6421a6f2df47906
BOLIVAR\1053793623 <ivan.barreiro@segurosbolivar.com>
2022-08-01 10:33:05 -0500

    Creacion de la entidad Producto y clase Response

---
+++ Application/Features/Productos/Commands/CreateProductoCommand/CreateProductoCommand.cs
@@ -0,0 +1,17 @@
+﻿using Application.Wrappers;
+using MediatR;
+using System;
+
+namespace Application.Features.Productos.Commands.CreateProductoCommand
+{
+    public class CreateProductoCommand : IRequest<Response<int>>
+    {
+        public string Descripcion { get; set; }
+        public string Estado { get; set; }
+        public DateTime FechaFabricacion { get; set; }
+        public DateTime FechaValidez { get; set; }
+        public int CodigoProveedor { get; set; }
+        public string DescripcionProveedor { get; set; }
+        public string TelefonoProveedor { get; set; }
+    }
+}
---
+++ Domain/Entities/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Producto
+    {
+        public int Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public string Estado { get; set; }
+        public DateTime FechaFabricacion { get; set; }
+        public DateTime FechaValidez { get; set; }
+        public int CodigoProveedor { get; set; }
+        public string DescripcionProveedor { get; set; }
+        public string TelefonoProveedor { get; set; }
+    }
+}
