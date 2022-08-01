f347daa374b7b23777deae25fd1544a7efd24323
BOLIVAR\1053793623 <ivan.barreiro@segurosbolivar.com>
2022-08-01 02:42:02 -0500

    Canalizador que permite manejo de excepciones de las reglas de negocio que se definan, al enviar peticiones.

---
+++ Application/Behaviours/ValidationBehavior.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Behaviours
+{
+    //Interface propia de MediatR para la implementacion de los Handle o manejaores
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        //Permite validar todo el contenido que ingrese a traves de la WebAPI y canalizarlo antes de que entre al manejar principal
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validators.Any())
+            {
+                var context = new FluentValidation.ValidationContext<TRequest>(request);
+                var validationResult = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                var failures = validationResult.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+                if (failures.Count != 0)
+                {
+                    throw new Exceptions.ValidationException(failures);
+                }
+            }
+            return await next();
+        }
+    }
+}
